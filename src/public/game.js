9b4115ac015a3f9862802ac315e8db06f33bef85
Gabriell04 <105636474+Gabriell04@users.noreply.github.com>
2022-06-29 11:21:59 +0100

    Add files via upload

--- src/public/game.js
+++ src/public/game.js
@@ -157,4 +157,4 @@ export default function createGame() {
         subscribe,
         start
     }
-}
+} 
--- src/public/render-screen.js
+++ src/public/render-screen.js
@@ -1,24 +1,27 @@
 export default function renderScreen(screen, game, requestAnimationFrame, currentPlayerId) {
     const context = screen.getContext('2d')
-    context.fillStyle = 'white'
+    context.fillStyle = '#444444'
     context.clearRect(0, 0, 50, 50)
 
+    //player
     for (const playerId in game.state.players) {
         const player = game.state.players[playerId]
-        context.fillStyle = 'black'
+        context.fillStyle = '#CCCCCC'
         context.fillRect(player.x, player.y, 1, 1)
     }
 
+    //fruit
     for (const fruitId in game.state.fruits) {
         const fruit = game.state.fruits[fruitId]
-        context.fillStyle = 'green'
+        context.fillStyle = '#d01050'
         context.fillRect(fruit.x, fruit.y, 1, 1)
     }
 
     const currentPlayer = game.state.players[currentPlayerId]
 
+    //currentPlayer
     if(currentPlayer) {
-        context.fillStyle = '#F0DB4F'
+        context.fillStyle = '#80e040'
         context.fillRect(currentPlayer.x, currentPlayer.y, 1, 1)
     }
 
