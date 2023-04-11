fab77704de0b0cd7e334bd1028fda9f3876b533e
Gabriell04 <105636474+Gabriell04@users.noreply.github.com>
2023-04-11 10:50:22 +0100

    Update keyboard-listener.js

--- src/public/keyboard-listener.js
+++ src/public/keyboard-listener.js
@@ -1,39 +1,48 @@
 export default function createKeyboardListener(document) {
     const state = {
-        observers: [],
-        playerId: null
-    }
-
+      observers: [], // array de funções observadoras
+      playerId: null // ID do jogador
+    };
+  
+    // Registra o ID do jogador
     function registerPlayerId(playerId) {
-        state.playerId = playerId
+      state.playerId = playerId;
     }
-
+  
+    // Inscreve uma função observadora
     function subscribe(observerFunction) {
-        state.observers.push(observerFunction)
+      state.observers.push(observerFunction);
     }
-
+  
+    // Cancela a inscrição de todas as funções observadoras
+    function unsubscribeAll() {
+      state.observers = []; // atribui uma nova matriz vazia
+    }
+  
+    // Notifica todas as funções observadoras com um comando
     function notifyAll(command) {
-        for (const observerFunction of state.observers) {
-            observerFunction(command)
-        }
+      // usa o spread operator para copiar o array de observadores
+      // e evitar a mutação do objeto de estado original
+      [...state.observers].forEach(observerFunction => {
+        observerFunction(command);
+      });
     }
-
-    document.addEventListener('keydown', handleKeydown)
-
-    function handleKeydown(event) {
-        const keyPressed = event.key
-
-        const command = {
-            type: 'move-player',
-            playerId: state.playerId,
-            keyPressed
-        }
-
-        notifyAll(command)
-    }
-
+  
+    // Adiciona um ouvinte de teclado que notifica as funções observadoras
+    document.addEventListener("keydown", event => {
+      const keyPressed = event.key;
+      const command = {
+        type: "move-player",
+        playerId: state.playerId,
+        keyPressed
+      };
+      notifyAll(command);
+    });
+  
+    // Retorna um objeto com métodos para interagir com o ouvinte de teclado
     return {
-        subscribe,
-        registerPlayerId
-    }
-}
+      subscribe, // inscreve uma função observadora
+      unsubscribeAll, // cancela a inscrição de todas as funções observadoras
+      registerPlayerId // registra o ID do jogador
+    };
+  }
